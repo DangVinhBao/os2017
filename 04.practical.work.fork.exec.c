8007a10acbc2d572190b58b17cad7d890848ce74
DangVinhBao <sieusaoda@gmail.com>
2017-02-13 16:22:54 +0700

    Practical work 8 + Updated practical work 4

--- 04.practical.work.fork.exec.c
+++ 04.practical.work.fork.exec.c
@@ -3,25 +3,31 @@
 #include<stdlib.h>
 
 int main (){
+
+	printf("Main before fork()\n");
 	
 	int pid1 = fork();
 	if (pid1 == 0){
 	
-		printf("\nChild process 1:");
-		system("ps -ef");
+		printf("\nChild process run ps -ef:\n");
+		char *ps_cmd[] = {"ps", "-ef", NULL};
+		execvp("ps", ps_cmd);
 
-		int pid2 = fork();
-		if (pid2 == 0) {
-
-			printf("\nChild process 2:");
-			system("free -h");
-
-		}
-		else return 0;
 		
 	}
-	else return 0;
-		
+	
+	else{
+
+	int pid2 = fork();
+	if (pid2 == 0) {
+
+		printf("\nChild process run free -h:\n");
+		char *free_cmd[] = {"free", "-h", NULL};
+		execvp("free", free_cmd);
+
+	}
+
+	}
 
 	return 0;
 
